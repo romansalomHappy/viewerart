f2f33a8277d718733a69c52e02b227f38e422659
romansalomHappy <roman@happyagencies.com>
2024-08-23 11:02:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
+    <iframe
+      src="https://view.art/"
+      style={{ width: '100%', height: '100%', border: 'none' }}
+      title="Web Viewer"
+    />
+  </div>
   );
 }
 
